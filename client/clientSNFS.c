1dba1264f7a513dd3940a262b8a125bc4505d6ec
faiq <faiqrazarizvi@gmail.com>
2014-11-25 00:08:47 -0500

    starts minimum of client

---
+++ client/clientSNFS.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <sys/socket.h> 
+#include <netinet/ip.h>
+#include <sys/types.h>
+
+void setServer(char * serverIP, int port) {
+  struct hostnet * hostEntry; 
+  if ((hostEntry = gethostbyname ( serverIP )) == (struct hostent*) NULL)
+    return 0;
+  
+}
