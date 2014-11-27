4154974a79c1f4593f9dc8f753beadb5f0154b42
faiq <faiqrazarizvi@gmail.com>
2014-11-26 21:41:23 -0500

    finishes setServer method

--- client/clientSNFS.c
+++ client/clientSNFS.c
@@ -4,12 +4,44 @@
 #include <sys/socket.h> 
 #include <netinet/ip.h>
 #include <sys/types.h>
+#include <errno.h>
 
 
-int socketFd; 
-void setServer(char * serverIP, int port) {
-  struct hostnet * hostEntry; 
-  if ((hostEntry = gethostbyname ( serverIP )) == (struct hostent*) NULL)
+int socketFd; //global to save the file descriptor for the socket
+struct sockaddr_in servAddr; //global to save address of the server
+
+static int host2IpAddr(char *anIpName){
+
+  struct hostent *hostEntry;
+  struct in_addr *scratch;
+ 
+  if ((hostEntry = gethostbyname ( anIpName )) == (struct hostent*) NULL)
     return 0;
+ 
+  scratch = (struct in_addr *) hostEntry->h_addr;
+ 
+  return (ntohl(scratch->s_addr));
+   
+}
+
+void setServer(char * serverIP, int port) {
+
+  socketFd = socket (AF_INET, SOCK_STREAM, 0); //create a new socket connection
+
+  if (socketFd < 0) { 
+    printf("error creating client socket, error%d\n",errno);
+    perror("meaning:"); exit(0);
+  }
+  
+  memset (&servAddr, 0, sizeof (servAddr));
+  servAddr.sin_addr.s_addr = htonl (host2IpAddr(serverIP));
+  servAddr.sin_port = htons (port);
+  servAddr.sin_family = AF_INET;
+  int con = connect (socketFd, (struct sockaddr *)&servAddr, sizeof (serv_ad));
+  
+  if (con < 0) {
+    printf("error creating client socket, error%d\n",errno);
+    perror("meaning:"); exit(0);
+  }
   
 }
