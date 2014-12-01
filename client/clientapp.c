c4643f467ac8dba2f971f8cc627b5c0ae6f986c7
faiq <faiqrazarizvi@gmail.com>
2014-12-01 02:32:51 -0500

    finishes sample program

--- client/clientapp.c
+++ client/clientapp.c
@@ -1,16 +1,49 @@
 #include "clientSNFS.h" 
 #include <stdio.h>  
+#include <stdlib.h>
+#include <string.h> 
+
+void revstr(char * str) {
+  int right = strlen(str) - 1;
+  int left = 0;
+  while (left < right) {
+    char c = str[right];
+    str[right] = str[left];
+    str[left] = c;
+    ++left;
+    --right;
+  }
+}
 
 int main (int argc, char ** argv)  {
-  setServer("127.0.0.1", 8124);
-  int x = openFile ("yo.txt");
-	printf("this is x %d\n", x);
-  char buff[1024];
-  int readRes = readFile (x, buff);
-  printf ("this is buff %s\n", buff);
-  int z = writeFile (x, "How can I find out the size of a file? I opened with an application written in C. I would like to know the size");
-  fileStat stuff; 
-  int a = statFile(x, &stuff);
-  closeFile(x);
+  if (argc <  4) { 
+    printf ("not enough arguments\n");
+    exit (1);
+  }
+
+  char * server = argv[1];
+  int port = atoi(argv[2]);
+  char * file = argv[3]; 
+  
+  setServer(server, port);  
+  int infd = openFile ("file.in");
+  int filefd = openFile(file); 
+  fileStat stat; 
+  statFile (filefd, &stat); 
+  char * buffer = malloc (stat.file_size); 
+  readFile (filefd, buffer); 
+  writeFile (infd, buffer); 
+  statFile (infd, &stat);
+  int rfd = openFile ("reverse.in");
+  readFile (infd, buffer); 
+  revstr (buffer); 
+  writeFile (rfd, buffer);
+  statFile (rfd, &stat);   
+
+  closeFile(rfd);
+  closeFile(filefd);
+  closeFile (infd);
+
+  free (buffer);
   return 0;
 }
